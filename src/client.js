0da243d689588c8e49f1988f25f5c534692ee4d5
ybalexdp <ybalexdp@gmail.com>
2020-03-08 17:33:36 +0900

    Updates

--- src/client.js
+++ src/client.js
@@ -5,3 +5,13 @@ import App from './App';
 var ReactRouter = require('react-router');
 
 ReactDOM.render(<App />, document.getElementById('app'));
+/*
+const app = document.getElementById('app');
+ReactDOM.render(
+  <Router>
+    <Layout>
+      <Route path="/about" component={About}></Route>
+    </Layout>
+  </Router>,
+app);
+*/
