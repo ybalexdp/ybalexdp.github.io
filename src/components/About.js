30606a10b3c4d95b92b21dc6d0765382803a9185
ybalexdp <ybalexdp@gmail.com>
2021-05-23 12:12:40 +0900

    update

--- src/components/About.js
+++ src/components/About.js
@@ -9,19 +9,19 @@ export default class About extends React.Component {
         <p>Tech Lead at MyRefer,Inc</p>
         <p>
           HR Tech系のベンチャーでTech
-          Leadとして働いています。主戦場はインフラ/バックエンドです。フロントは業務でReactを扱っていますが、このサイトを見ればわかるように得意ではないですw
+          Leadとして働いています。主戦場はインフラ/バックエンドです。
         </p>
         <p>
           最近はスクラムマスターや採用/マネジメント業の割合も増えていますが、SRE/Micorservicesといったキーワードに興味があります。
         </p>
         <h2>Skills</h2>
         <h3>Programming languages/Framework</h3>
-        <p>go, php, python, C, HTML/CSS/Javascript, jQuery, node.js, FuelPHP, React.js</p>
+        <p>go, php, python, hack/hhvm, C, HTML/CSS/Javascript, jQuery, node.js, FuelPHP, React, TypeScript</p>
         <h3>Infrastructure</h3>
         <h4>AWS</h4>
-        <p>EC2, S3, ECS, EKS, Fargate, Athena, Glue, Lambda, Aurora</p>
+        <p>EC2, S3, EKS, Athena, Glue, Lambda, Aurora, CloudFront, etc</p>
         <h4>Middleware/Monitering/CICD/etc</h4>
-        <p>Sensu, Datadog, Grafana, Fluentd, ElasticSearch, Kibana, OpenStack, Jenkins, Github Actions</p>
+        <p>gRPC, Sensu, Datadog, Grafana, Fluentd, ElasticSearch, Kibana, OpenStack, Jenkins, Github Actions</p>
         <h4>Networking</h4>
         <p>VXLAN, OvS, DPDK</p>
         <h3>Certification</h3>
@@ -40,6 +40,16 @@ export default class About extends React.Component {
           <li>
             <a href="https://flxy.jp/article/8046" target="_blank">
               flexy
+            </a>
+          </li>
+          <li>
+            <a href="https://note.com/myrefer/n/n10e27f52f315" target="_blank">
+              社内対談記事(前編)
+            </a>
+          </li>
+          <li>
+            <a href="https://note.com/myrefer/n/n242e494e7e02" target="_blank">
+              社内対談記事(後編)
             </a>
           </li>
         </ul>
