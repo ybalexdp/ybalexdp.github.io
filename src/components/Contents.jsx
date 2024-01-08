31157b81b6ce469b2425b29c6d9b8087aafe0352
ybalexdp <ybalexdp@gmail.com>
2024-01-09 00:25:28 +0900

    v2.0.0 release

---
+++ src/components/Contents.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {  Link } from 'react-router-dom';
+
+export const Contents = () => {
+
+    return (
+      <div class="about">
+        <Link to="/about">about</Link>
+      </div>
+    );
+
+}
